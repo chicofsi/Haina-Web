9fcd4a3159c311e1f50ecb536361510d81bbdbbc
Griffin <griffin.seannery@yahoo.com>
2021-06-03 16:33:45 +0700

    ubah jadi get news

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -21,13 +21,10 @@ class NewsController extends Controller
 	//newsapi.ai
 	public function __construct()
     {
-        $apiKey="9058efb9-8314-4283-8465-6bb0c2a19116";
+        //$apiKey="9058efb9-8314-4283-8465-6bb0c2a19116";
         $this->client = new Client([
             'base_uri' => 'http://eventregistry.org/api/v1/article/',
-            'timeout'  => 100.0,
-            'headers' => [
-                'apiKey' => $apiKey,
-            ]
+            'timeout'  => 100.0
         ]);
     }
 
@@ -55,14 +52,8 @@ class NewsController extends Controller
         ];
 
         $response=$this->client->request(
-            'POST',
-            'getArticles',
-            [
-                'form_params' => $body,
-                'on_stats' => function (TransferStats $stats) use (&$url) {
-                    $url = $stats->getEffectiveUri();
-                }
-            ]  
+            'GET',
+            'getArticles?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22locationUri%22%3A%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FIndonesia%22%7D%2C%7B%22lang%22%3A%22zho%22%7D%5D%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&dataType=news&resultType=articles&articlesSortBy=date&articlesCount=10&includeArticleCategories=true&includeArticleLocation=true&includeArticleImage=true&includeArticleVideos=true&articleBodyLen=-1&includeConceptImage=true&includeConceptDescription=true&includeSourceDescription=true&includeSourceLocation=true&apiKey=9058efb9-8314-4283-8465-6bb0c2a19116&callback=JSON_CALLBACK'
         );
 
         $result = $response->getBody()->getContents();
