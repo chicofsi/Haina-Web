54e76cca86426a5602c6abd90ba81ca1c627eebe
Griffin <griffin.seannery@yahoo.com>
2021-06-07 14:20:56 +0700

    add payment instructions

---
+++ app/Models/HowToPay.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HowToPay extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'how_to_pay';
+
+    protected $fillable = [ 
+        'id_payment_method', 'payment_media', 'how_to'
+    ];
+
+    public function payment_method(){
+        return $this->belongsTo('App\Models\PaymentMethod', 'id', 'id_payment_method');
+    }
+    
+
+}
