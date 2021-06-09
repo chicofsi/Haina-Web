765b09baf054f0c26aaa3dfb239859e381a8ccc4
Griffin <griffin.seannery@yahoo.com>
2021-06-09 12:16:10 +0700

    Update PersonalAccessToken.php

---
+++ app/Models/PersonalAccessToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PersonalAccessToken extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'personal_access_token';
+
+    protected $fillable = [
+        'tokenable_type', 'tokenable_id', 'name', 'token', 'abilities'
+    ];
+
+
+}
