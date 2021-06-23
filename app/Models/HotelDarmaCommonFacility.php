8a4c637bd86ee5edf10f02d3e751758de4cfd789
Griffin <griffin.seannery@yahoo.com>
2021-06-23 15:56:44 +0700

    Update HotelDarmaCommonFacility.php

--- app/Models/HotelDarmaCommonFacility.php
+++ app/Models/HotelDarmaCommonFacility.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HotelDarmaFacilitiesGroup extends Model
+class HotelDarmaCommonFacility extends Model
 {
     use HasFactory;
 
