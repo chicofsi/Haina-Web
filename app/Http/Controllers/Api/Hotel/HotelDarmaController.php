ecb5fa52d2baacb7383336b935664ac9cada8d6d
Griffin <griffin.seannery@yahoo.com>
2021-06-30 12:11:18 +0700

    Update HotelDarmaController.php

--- app/Http/Controllers/Api/Hotel/HotelDarmaController.php
+++ app/Http/Controllers/Api/Hotel/HotelDarmaController.php
@@ -1332,7 +1332,7 @@ class HotelDarmaController extends Controller
 
                     }
 
-                    $value->special_request = (object) $special_request;
+                    $value->special_request = (array)$special_request;
                 }
                 else{
                     $value->special_request = $value->requests;
