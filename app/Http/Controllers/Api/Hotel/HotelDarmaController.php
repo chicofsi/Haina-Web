8b84c7d8e6dce7bd8bc2d6b79d89370d9f7228d5
Griffin <griffin.seannery@yahoo.com>
2021-07-01 08:34:32 +0700

    Update HotelDarmaController.php

--- app/Http/Controllers/Api/Hotel/HotelDarmaController.php
+++ app/Http/Controllers/Api/Hotel/HotelDarmaController.php
@@ -1316,7 +1316,7 @@ class HotelDarmaController extends Controller
 
                 $hotel = HotelDarma::where('id', $value->hotel_id)->first();
                 
-                /*
+                
                 if($hotel['request_array'] == true){
                     $request_id = explode(',', $value->requests);
                     $special_request = [];
@@ -1338,7 +1338,7 @@ class HotelDarmaController extends Controller
                 else{
                     $value->special_request = $value->requests;
                 }
-                */
+                
 
                 //$value->special_request = "obj";
             }
