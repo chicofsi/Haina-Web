32cc6cd1e1ed27937c1b05c7b3d86720388bc55c
Griffin <griffin.seannery@yahoo.com>
2021-07-05 10:46:05 +0700

    Update PropertyTransaction.php

--- app/Models/PropertyTransaction.php
+++ app/Models/PropertyTransaction.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PropertyData extends Model
+class PropertyTransaction extends Model
 {
     use HasFactory;
 
