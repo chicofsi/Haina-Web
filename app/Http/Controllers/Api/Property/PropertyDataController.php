030c64c5600b72c590c3dc141aada9ec445f9382
Griffin <griffin.seannery@yahoo.com>
2021-07-06 17:04:35 +0700

    Update PropertyDataController.php

--- app/Http/Controllers/Api/Property/PropertyDataController.php
+++ app/Http/Controllers/Api/Property/PropertyDataController.php
@@ -174,7 +174,7 @@ class PropertyDataController extends Controller
                     array_push($property_facility, $facility);
                 }
 
-                //dd($property_facility);
+                dd($property_facility);
                 $value->facilities = $property_facility;
             }
 
