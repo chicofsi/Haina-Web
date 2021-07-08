9e85351f43460458309d32437327d768698f340e
Griffin <griffin.seannery@yahoo.com>
2021-07-08 11:36:16 +0700

    Update HotelDarmaController.php

--- app/Http/Controllers/Api/Hotel/HotelDarmaController.php
+++ app/Http/Controllers/Api/Hotel/HotelDarmaController.php
@@ -1550,6 +1550,10 @@ class HotelDarmaController extends Controller
                         'sink' => storage_path('app/public/hotel'.str_replace(' ','-', 'hotel_'.$hotel['hotel_name'].'_'.substr($image_id, -1)).'.jpeg')
                     ]
                 );
+
+                $bodyresponse=json_decode($response->getBody()->getContents());
+
+                return $bodyresponse;
             }
             catch(RequestException $e) {
                 if ($e->hasResponse()) {
