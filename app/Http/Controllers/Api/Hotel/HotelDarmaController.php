0a444472c3240a072fa83d56b7e8b5084dc07f71
Griffin <griffin.seannery@yahoo.com>
2021-07-08 16:54:45 +0700

    Update HotelDarmaController.php

--- app/Http/Controllers/Api/Hotel/HotelDarmaController.php
+++ app/Http/Controllers/Api/Hotel/HotelDarmaController.php
@@ -1559,7 +1559,7 @@ class HotelDarmaController extends Controller
                     'hotel/Image?ID='.$image_id,
                     [
                         //'sink' => $temp
-                        'sink' => public_path('hotel/'.str_replace(' ','-', 'hotel_'.$hotel['hotel_name'].'_'.substr($image_id, -1)).'.jpeg')
+                        'sink' => public_path('images/hotel/'.str_replace(' ','-', 'hotel_'.$hotel['hotel_name'].'_'.substr($image_id, -1)).'.jpeg')
                     ]
                 );
 
