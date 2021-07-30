5225d835eea5a712d8b5bb640af5744dcd4d3174
Griffin <griffin.seannery@yahoo.com>
2021-07-30 16:08:47 +0700

    Update ForumController.php

--- app/Http/Controllers/Api/Forum/ForumController.php
+++ app/Http/Controllers/Api/Forum/ForumController.php
@@ -698,7 +698,7 @@ class ForumController extends Controller
         $comment = array_column($threads, 'comment_count');
 
         $threads = array_multisort($like, SORT_DESC, $comment, SORT_DESC, $threads);
-
+        dd($threads);
         $hot_threads = array_slice($threads, 0, 5);
 
         if(count($hot_threads) > 0){
