c8bfe321277fef4d8f802650be43c719ddc9bf45
Griffin <griffin.seannery@yahoo.com>
2021-08-16 14:22:56 +0700

    Update ForumController.php

--- app/Http/Controllers/Api/Forum/ForumController.php
+++ app/Http/Controllers/Api/Forum/ForumController.php
@@ -1705,7 +1705,9 @@ class ForumController extends Controller
 
     public function showHotThreads(Request $request){
 
-        $list_post = ForumPost::where('deleted_at', null)->with('comments', 'images', 'videos')->get();
+        $list_post = ForumPost::where('deleted_at', null)->with(['comments' => function($q){
+            $q->where('forum_comment.deleted_at', '=', null);
+        }], 'images', 'videos')->get();
         $hot_threads = [];
         $threads = [];
 
