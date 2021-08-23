2783d413ce8467b40b3fdd4ac35dd3983b2dc36a
Griffin <griffin.seannery@yahoo.com>
2021-08-23 11:06:25 +0700

    Update PulsaController.php

--- app/Http/Controllers/Pulsa/PulsaController.php
+++ app/Http/Controllers/Pulsa/PulsaController.php
@@ -250,31 +250,50 @@ class PulsaController extends Controller
         ];
         try {
 
-            $response=$this->client->request(
-                'POST',
-                'inquirytransaction',
-                [
-                    'form_params' => $body
-                ]  
-            );
-            //return $response;
+            $checkfive = Transaction::where('order_id', $request->order_id)->where('product_code', $request->product_code)->orderBy('created_at', 'desc')->first();
 
-            $bill = json_decode($response->getBody()->getContents());
-            $bill->product_code = $request->product_code;
+            if($checkfive){
+                $startdate = date("Y-m-d h:i:s");
+                $minutediff = $startdate->diff(new DateTime($checkfive['created_at']));
 
-            if(isset($bill->data->bill_period)){
-                $bill->data->bill_date = $bill->data->bill_period;
-                unset($bill->data->bill_period);
+                $minutes = $minutediff->days * 24 * 60;
+                $minutes += $minutediff->h * 60;
+                $minutes += $minutediff->i;
+
+                if($minutes <= 5){
+                    return response()->json(new ValueMessage(['value'=>0,'message'=>'Please wait 5 minutes for the same transaction!','data'=> '']), 401);
+                }
+                else{
+                    
+                    $response=$this->client->request(
+                        'POST',
+                        'inquirytransaction',
+                        [
+                            'form_params' => $body
+                        ]  
+                    );
+                    //return $response;
+
+                    $bill = json_decode($response->getBody()->getContents());
+                    $bill->product_code = $request->product_code;
+
+                    if(isset($bill->data->bill_period)){
+                        $bill->data->bill_date = $bill->data->bill_period;
+                        unset($bill->data->bill_period);
+                    }
+
+                    if(isset($bill) && $bill->error_code == 0000){
+                        $billdata = new BillResource($bill);
+
+                        return response()->json(new ValueMessage(['value'=>1,'message'=>'Bill Details Found!','data'=> $billdata]), 200);
+                    }
+                    else{
+                        return response()->json(new ValueMessage(['value'=>0,'message'=>$bill->error_desc,'data'=> $bill->error_code]), 500);
+                    }
+                }
+
             }
 
-            if(isset($bill) && $bill->error_code == 0000){
-                $billdata = new BillResource($bill);
-
-                return response()->json(new ValueMessage(['value'=>1,'message'=>'Bill Details Found!','data'=> $billdata]), 200);
-            }
-            else{
-                return response()->json(new ValueMessage(['value'=>0,'message'=>$bill->error_desc,'data'=> $bill->error_code]), 500);
-            }
             
         }catch(RequestException $e) {
             echo Psr7\Message::toString($e->getRequest());
