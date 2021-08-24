098aeaafbe22c28da50cb407a06c8466ceb168d0
Griffin <griffin.seannery@yahoo.com>
2021-08-24 15:06:22 +0700

    Update MidtransController.php

--- app/Http/Controllers/Api/Midtrans/MidtransController.php
+++ app/Http/Controllers/Api/Midtrans/MidtransController.php
@@ -306,14 +306,16 @@ class MidtransController extends Controller
             if($transaction_status=='settlement'){
                 $settlement_time=date("Y-m-d H:m:s",strtotime($request->settlement_time));
                 if($transaction['package'] == 'basic'){
-                    $newtime = date_add($settlement_time, date_interval_create_from_date_string('30 days'));
+                    $set_time = new DateTime($settlement_time);
+                    $newtime = date_add($set_time, date_interval_create_from_date_string('30 days'));
 
                     $update_expiry = JobVacancy::where('id', $order_id[2])->update([
                         'deleted_at' => $newtime
                     ]);
                 }
                 else if($transaction['package'] == 'best'){
-                    $newtime = date_add($settlement_time, date_interval_create_from_date_string('60 days'));
+                    $set_time = new DateTime($settlement_time);
+                    $newtime = date_add($set_time, date_interval_create_from_date_string('60 days'));
 
                     $update_expiry = JobVacancy::where('id', $order_id[2])->update([
                         'deleted_at' => $newtime
