bfe4234bed050e7f98401b9155a4a55ce8a7a6a9
Griffin <griffin.seannery@yahoo.com>
2021-08-30 20:28:28 +0700

    Update PulsaController.php

--- app/Http/Controllers/Pulsa/PulsaController.php
+++ app/Http/Controllers/Pulsa/PulsaController.php
@@ -952,7 +952,7 @@ class PulsaController extends Controller
 
                 $ad_list = [
                     'order_id' => $get_payment['order_id'],
-                    'transaction_time' => $get_payment['created_at'],
+                    'transaction_time' => $get_payment['created_at']->toDateTimeString(),
                     'product' => $value->position." Ad ".$package_name['name'],
                     'total_amount' => $get_payment['price'],
                     'status' => $get_payment['payment_status'],
