e890f76830dfc70f64b76a73b95c7bde351d1d46
Griffin <griffin.seannery@yahoo.com>
2021-09-06 14:16:34 +0700

    email verif routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\Api\WebHooks;
 use App\Http\Controllers\Admin\UserNotification\ManageNotificationCategory;
 use App\Http\Controllers\Admin\UserNotification\ManageNotification;
 use App\Http\Controllers\Api\Midtrans\MidtransController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 
 /*
@@ -119,6 +121,18 @@ Route::group(['prefix' => 'dashboard','middleware' =>'auth:admin'], function() {
     Route::get('/email/verify', function () {
         return view('auth.verify-email');
     })->middleware('auth')->name('verification.notice');
+
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+    
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+    
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 });
 
