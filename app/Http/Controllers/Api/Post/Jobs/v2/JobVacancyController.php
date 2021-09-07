5da9550d22c242ac0da6bf756f4bb3154794b6e1
Griffin <griffin.seannery@yahoo.com>
2021-09-07 16:35:08 +0700

    Update JobVacancyController.php

--- app/Http/Controllers/Api/Post/Jobs/v2/JobVacancyController.php
+++ app/Http/Controllers/Api/Post/Jobs/v2/JobVacancyController.php
@@ -445,10 +445,9 @@ class JobVacancyController extends Controller
                 }
                 else{
                     $applicant = JobVacancyApplicant::where('id_vacancy', $request->id_vacancy)->where('status', 'applied')->with('user.education', 'user.work_experience')->get();
-                    
+
                     foreach($applicant as $key => $value){
-                        dd($value);
-                        $edu_name = Education::where('id', $value->user->education['id'])->first();
+                        $edu_name = Education::where('id', $value->user->education->id)->first();
 
                         $value->user->education->edu_level = $edu_name['name'];
                     }
