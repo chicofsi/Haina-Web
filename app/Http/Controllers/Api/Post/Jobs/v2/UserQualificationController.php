7e39fac19469ef8059a621fbd5f06808ca8a7866
Griffin <griffin.seannery@yahoo.com>
2021-09-09 15:24:40 +0700

    Update UserQualificationController.php

--- app/Http/Controllers/Api/Post/Jobs/v2/UserQualificationController.php
+++ app/Http/Controllers/Api/Post/Jobs/v2/UserQualificationController.php
@@ -98,7 +98,6 @@ class UserQualificationController extends Controller
             'year_start' => 'nullable|integer',
             'year_end' => 'nullable|integer',
             'gpa' => 'numeric|nullable',
-            'major' => 'numeric|nullable',
             'id_edu' => 'nullable|integer'
         ]);
     
@@ -123,7 +122,10 @@ class UserQualificationController extends Controller
                         return response()->json(new ValueMessage(['value'=>0,'message'=>'Education level cannot be lower','data'=> '']), 401);
                     }
                     else if($id_edu > 1 && $request->gpa == null){
-                        return response()->json(new ValueMessage(['value'=>0,'message'=>'GPA must be filled if education is not high school','data'=> '']), 401);
+                        return response()->json(new ValueMessage(['value'=>0,'message'=>'GPA must be filled if education is not high school (1)','data'=> '']), 401);
+                    }
+                    else if($id_edu > 1 && $request->major == null){
+                        return response()->json(new ValueMessage(['value'=>0,'message'=>'Major must be filled if education is not high school (1)','data'=> '']), 401);
                     }
                     else{
                         $update_edu = UserEducation::where('id_user', Auth::id())->update([
