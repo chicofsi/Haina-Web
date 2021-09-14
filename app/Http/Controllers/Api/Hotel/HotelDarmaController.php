91068e491ddb0627d1e51a7b970fc182992f65cb
Griffin <griffin.seannery@yahoo.com>
2021-09-14 15:19:10 +0700

    Update HotelDarmaController.php

--- app/Http/Controllers/Api/Hotel/HotelDarmaController.php
+++ app/Http/Controllers/Api/Hotel/HotelDarmaController.php
@@ -1462,10 +1462,10 @@ class HotelDarmaController extends Controller
                 $request = new Request();
                 $request->agent_os_ref = $valuepro->agent_os_ref;
                 $check_status = $this->getBookingDetail($request);
-                dd($check_status);
+                //dd($check_status);
                 //ErrorException: Trying to get property 'voucherNo' of non-object
 
-                if($check_status->voucherNo != null){
+                if(isset($check_status->voucherNo)){
                     $update_booking = HotelDarmaBooking::where('id', $valuepro->id)->update([
                         'status' => 'success',
                         'reservation_no' => $check_status->data->voucherNo
