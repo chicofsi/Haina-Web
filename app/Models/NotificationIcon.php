42fb35ac6bcff0ad1cc7ac621369947f3f9e3aec
Griffin <griffin.seannery@yahoo.com>
2021-09-21 11:20:45 +0700

    Create NotificationIcon.php

---
+++ app/Models/NotificationIcon.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotificationIcon extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'notification_icon';
+
+    protected $fillable = [
+        'name','icon'
+    ];
+
+
+}
