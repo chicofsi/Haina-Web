7331a2452b47c25dab282eab789f44e45f03655c
Griffin <griffin.seannery@yahoo.com>
2021-09-22 17:07:35 +0700

    Update ForumController.php

--- app/Http/Controllers/Api/Forum/ForumController.php
+++ app/Http/Controllers/Api/Forum/ForumController.php
@@ -528,8 +528,8 @@ class ForumController extends Controller
                     $post = ForumPost::where('id', $request->post_id)->first();
 
                     $checkmod = ForumMod::where('user_id', $value->user_id)->where('subforum_id', $post['subforum_id'])->first();
-                    $checkban = ForumBan::where('subforum_id', $request->subforum_id)->where('user_id', $value->user_id)->first();
-                    dd($checkban);
+                    $checkban = ForumBan::where('subforum_id', $post['subforum_id'])->where('user_id', $value->user_id)->first();
+                    
                     if($checkban){
                         $value->mod = "banned";
                     }
