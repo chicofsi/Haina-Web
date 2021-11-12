38a4e30af9daf1fa0c51f417ef67bf1f34a4dec6
Griffin <griffin.seannery@yahoo.com>
2021-11-12 10:55:43 +0700

    Update CompanyController.php

--- app/Http/Controllers/Api/Company/CompanyController.php
+++ app/Http/Controllers/Api/Company/CompanyController.php
@@ -110,8 +110,8 @@ class CompanyController extends Controller
                 $company_list = $company_data;
 
                 if($request->sort_by_name == "asc"){
-                    $company_list = collect($company_list)->sortBy('name');
-                    $company_list->values()->toArray();
+                    $company_list = collect($company_list)->sortBy('name')->toArray();
+                    dd($company_list);
                 }
                 else if($request->sort_by_name == "desc"){
                     $company_list = collect($company_list)->sortByDesc('name')->toArray();
