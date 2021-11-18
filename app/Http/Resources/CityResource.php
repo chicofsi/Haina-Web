8c7c518fe4cc0b0ea1d7e8332f33864a0d6be719
Griffin <griffin.seannery@yahoo.com>
2021-11-18 11:38:09 +0700

    Update CityResource.php

--- app/Http/Resources/CityResource.php
+++ app/Http/Resources/CityResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+use App\Models\Province;
+
 class CityResource extends JsonResource
 {
     /**
@@ -12,11 +14,19 @@ class CityResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
+
     public function toArray($request)
     {
+
+        $province = Province::where('id', $this->id_province)->first();
+
         return [
             'id' => $this->id,
-            'name' => $this->name
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'id_province' => $this->id_province,
+            'province' => $province['name']
         ];
     }
 }
